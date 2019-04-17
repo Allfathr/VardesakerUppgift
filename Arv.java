a98804e6d4269ae9b3bb6ca07deab33705733963
Anton <31654921+Allfathr@users.noreply.github.com>
2019-04-17 13:48:43 +0200

    Add files via upload

---
+++ Arv.java
@@ -0,0 +1,106 @@
+// Anton Sandström 960726-4430
+
+
+abstract class Vardesaker {
+	private String namn;
+
+	public abstract double nyttVarde();
+
+	public abstract String toString();
+
+	public Vardesaker(String namn) {
+		this.namn = namn;
+	}
+
+	public String getNamn() {
+		return namn;
+	}
+
+	public double getRealVarde() {
+		return nyttVarde() * 1.25;
+	}
+}
+
+class Smycken extends Vardesaker {
+	private int adelstenar;
+	private double varde;
+	private boolean guld;
+
+	public Smycken(String namn, int adelstenar, boolean guld) {
+		super(namn);
+		this.adelstenar = adelstenar;
+		this.guld = guld;
+	}
+
+	@Override
+	public double nyttVarde() {
+		if (guld) {
+			varde = 2000 + (adelstenar * 500);
+		}
+
+		else if (!guld) {
+			varde = 700 + (adelstenar * 500);
+		}
+
+		return varde;
+	}
+
+	@Override
+	public String toString() {
+		String metall = null;
+		if (guld) {
+			metall = "guld";
+		} else if (!guld) {
+			metall = "silver";
+		}
+		return getNamn() + " av " + metall + " med " + adelstenar + " adelstenar," + " värd " + getRealVarde() + " kr.";
+	}
+}
+
+class Aktie extends Vardesaker {
+	private double kurs;
+	private int antal;
+
+	public Aktie(String namn, double kurs, int antal) {
+		super(namn);
+		this.kurs = kurs;
+		this.antal = antal;
+	}
+
+	@Override
+	public double nyttVarde() {
+		return antal * kurs;
+	}
+
+	@Override
+	public String toString() {
+		return getNamn() + " aktien har kursen: " + kurs + " du äger: " + antal + " st. Totalt värde är: "
+				+ getRealVarde();
+	}
+
+	public void setNyKurs(double nyKurs) {
+		kurs = nyKurs;
+	}
+}
+
+class Apparat extends Vardesaker {
+	private double inkopPris;
+	private double slitage;
+
+	public Apparat(String namn, double inkop, int slitage) {
+		super(namn);
+		inkopPris = inkop;
+		this.slitage = slitage;
+	}
+
+	@Override
+	public double nyttVarde() {
+		return inkopPris * (slitage / 10);
+	}
+
+	@Override
+	public String toString() {
+		return getNamn() + " köptes för: " + inkopPris + " och har ett slitage på: " + slitage + " värd nu: "
+				+ getRealVarde();
+	}
+}
---
+++ Gui.java
@@ -0,0 +1,79 @@
+// Anton Sandström ansa6928
+
+import java.util.*;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class Gui extends Application {
+	private TextArea area;
+	private RadioButton nameButton;
+	private RadioButton valueButton;
+	ArrayList<Vardesaker> allaSaker = new ArrayList<>();
+	
+	@Override
+	public void start(Stage primaryStage) {
+		Pane root = new FlowPane();
+		Label headLab = new Label("Värdesaker");
+		Scene scene = new Scene(root);
+		area = new TextArea();
+		Button show = new Button("Show");
+		show.setOnAction(new showHandler());
+		root.getChildren().addAll(headLab, area, show);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		
+		
+	}
+	
+	class showHandler implements EventHandler<ActionEvent> {
+		public void handle(ActionEvent event) {
+			area.setText("");
+			area.setText("This is working");
+			
+			if (nameButton.isSelected()) {
+				allaSaker.sort(new NameComparator());
+				for (Vardesaker sak : allaSaker) {
+					area.appendText(sak.toString());
+					area.appendText("\n");
+					
+				}
+				
+			}
+			
+			else if (valueButton.isSelected()) {
+				allaSaker.sort(new VardeComparator());
+				for (Vardesaker sak : allaSaker) {
+					area.appendText(sak.toString());
+					area.appendText("\n)");
+				}
+			}
+		}
+	}
+	
+	class NameComparator implements Comparator<Vardesaker> {
+		public int compare (Vardesaker v1, Vardesaker v2) {
+			return v1.getNamn().compareTo(v2.getNamn());
+		}
+	}
+	
+	class VardeComparator implements Comparator<Vardesaker> {
+		public int compare (Vardesaker v1, Vardesaker v2) {
+			return Double.compare(v2.getRealVarde(), v1.getRealVarde());
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
